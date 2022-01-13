db77056c152d8c358aba6e293a0931b9a50a06b1
Jaime Pardo <jdpm2014@gmail.com>
2022-01-13 14:32:56 -0300

    Creacion de aplicacion presupuesto en framework Angular

---
+++ src/app/ingreso/ingreso.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Ingreso } from './ingreso.model';
+import { IngresoServicio } from './ingreso.servicio';
+
+@Component({
+  selector: 'app-ingreso',
+  templateUrl: './ingreso.component.html',
+  styleUrls: ['./ingreso.component.css']
+})
+export class IngresoComponent implements OnInit {
+  ingresos:Ingreso [] =[]
+  
+
+
+  constructor(private ingresoServicio:IngresoServicio) { }
+
+  ngOnInit(): void {
+    this.ingresos = this.ingresoServicio.ingresos
+  }
+  eliminarRegistro(ingreso:Ingreso)
+  {
+    this.ingresoServicio.elminar(ingreso)
+  }
+
+
+}
